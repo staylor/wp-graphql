e4a58b119108deff01ba4a6acda9768afd37f6f1
Scott Taylor <scott.c.taylor@mac.com>
2017-06-02 16:10:41 -0400

    add raw field to comment content

--- src/schema/type/Content.js
+++ src/schema/type/Content.js
@@ -1,6 +1,7 @@
 import { GraphQLObjectType } from 'graphql';
 
 import rendered from 'field/rendered';
+import raw from 'field/raw';
 // 'protected' is a reserved word in JS
 import protectedField from 'field/protected';
 
@@ -9,6 +10,7 @@ const Content = new GraphQLObjectType({
   description: 'The content for the object.',
   fields: {
     ...rendered,
+    ...raw,
     ...protectedField,
   },
 });
