e16b253b231b9ab1c7e6d369381f77cccc9c5487
Scott Taylor <scott.c.taylor@mac.com>
2017-07-24 21:33:08 -0400

    support structured data for Title and Excerpt

--- src/schema/type/Excerpt.js
+++ src/schema/type/Excerpt.js
@@ -1,9 +1,10 @@
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLObjectType, GraphQLList } from 'graphql';
 
 import rendered from 'field/rendered';
 import raw from 'field/raw';
 // 'protected' is a reserved word in JS
 import protectedField from 'field/protected';
+import ContentNode from 'type/ContentNode';
 
 const Excerpt = new GraphQLObjectType({
   name: 'Excerpt',
@@ -12,6 +13,9 @@ const Excerpt = new GraphQLObjectType({
     ...rendered,
     ...raw,
     ...protectedField,
+    data: {
+      type: new GraphQLList(ContentNode),
+    },
   },
 });
 
--- src/schema/type/Title.js
+++ src/schema/type/Title.js
@@ -1,12 +1,16 @@
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLObjectType, GraphQLList } from 'graphql';
 
 import rendered from 'field/rendered';
+import ContentNode from 'type/ContentNode';
 
 const Title = new GraphQLObjectType({
   name: 'Title',
   description: 'The title for an object.',
   fields: {
     ...rendered,
+    data: {
+      type: new GraphQLList(ContentNode),
+    },
   },
 });
 
