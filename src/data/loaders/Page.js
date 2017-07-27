72fa7ebc6bd75dc4e4c521470343df2d814ac467
Scott Taylor <scott.c.taylor@mac.com>
2017-07-27 00:30:56 -0400

    ditch priming of pages

--- src/data/loaders/Page.js
+++ src/data/loaders/Page.js
@@ -6,29 +6,16 @@ import Page from 'data/Page';
 
 export default function getPageLoaders() {
   const endpoint = Page.getEndpoint();
-
-  let slugLoader;
   const pageLoader = new Dataloader(ids =>
     fetchData(endpoint, {
       qs: { include: ids, orderby: 'include', per_page: 100 },
-    }).then(({ data: { body: pages } }) => {
-      pages.forEach(page => {
-        slugLoader.prime(page.slug, page);
-      });
-      return pages;
-    })
+    }).then(({ data: { body } }) => body)
   );
-  slugLoader = new Dataloader(slugs =>
+  const slugLoader = new Dataloader(slugs =>
     fetchData(endpoint, { qs: { slug: slugs } })
       .then(({ data: { body } }) => body)
       // the REST API does not order by FIELD(slug, ....) yet
       .then(pages => slugs.map(slug => pages.find(page => slug === page.slug)))
-      .then(pages => {
-        pages.forEach(page => {
-          pageLoader.prime(page.id, page);
-        });
-        return pages;
-      })
   );
   return {
     load: async id => {
